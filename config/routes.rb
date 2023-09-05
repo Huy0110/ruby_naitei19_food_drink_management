29d8810e9328a5aa1acbfcc995bb64c9a63af6b9
Huy0110 <duchuy01102001@gmail.com>
2023-09-05 16:06:00 +0700

    Add demo 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root "static_pages#home"
-    resources :categories, param: :slug, only: [:index, :show]
+    resources :categories, param: :slug, only: [:show]
     resources :cuisines, param: :slug, only: [:index, :show]
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 # db/seeds.rb
 Category.create(name: "Category 1", slug: "category-1")
 Category.create(name: "Category 2", slug: "category-2")
+Category.create(name: "Category 3", slug: "category-3")
 
 Category.find_by(slug: "category-1").cuisines.create(
   name: "Cuisine 1",
@@ -11,11 +12,65 @@ Category.find_by(slug: "category-1").cuisines.create(
   available: true
 )
 
+Category.find_by(slug: "category-1").cuisines.create(
+  name: "Cuisine 12",
+  slug: "cuisine-12",
+  description: "Description for Cuisine 12",
+  price: 10,
+  discount: 0,
+  available: true
+)
+
+Category.find_by(slug: "category-1").cuisines.create(
+  name: "Cuisine 13",
+  slug: "cuisine-13",
+  description: "Description for Cuisine 13",
+  price: 10,
+  discount: 0,
+  available: true
+)
+
+Category.find_by(slug: "category-1").cuisines.create(
+  name: "Cuisine 14",
+  slug: "cuisine-14",
+  description: "Description for Cuisine 14",
+  price: 10,
+  discount: 0,
+  available: true
+)
+
+Category.find_by(slug: "category-1").cuisines.create(
+  name: "Cuisine 14",
+  slug: "cuisine-14",
+  description: "Description for Cuisine 14",
+  price: 10,
+  discount: 0,
+  available: true
+)
+
 Category.find_by(slug: "category-2").cuisines.create(
   name: "Cuisine 2",
   slug: "cuisine-2",
   description: "Description for Cuisine 2",
   price: 12,
+  discount: 0,
+  available: true
+)
+
+Category.find_by(slug: "category-3").cuisines.create(
+  name: "Cuisine 3",
+  slug: "cuisine-3",
+  description: "Description for Cuisine 3",
+  price: 22,
+  discount: 0,
+  available: true
+)
+
+Category.find_by(slug: "category-3").cuisines.create(
+  name: "Cuisine 31",
+  slug: "cuisine-31",
+  description: "Description for Cuisine 31",
+  price: 22,
   discount: 0,
   available: true
 )
